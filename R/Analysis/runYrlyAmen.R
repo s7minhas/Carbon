fdb4191b58def45381b3600cf79f1af6db23d30e
hermes829 <hermes829@gmail.com>
2015-07-21 23:31:38 -0700

    save smaller lat space

--- R/Analysis/runYrlyAmen.R
+++ R/Analysis/runYrlyAmen.R
@@ -24,10 +24,10 @@ foreach(yr = yrs, .packages=c("amen")) %dopar% {
 		Y=amData[[yr]][,,1:2], # 2, specifies defensive-entente alliance
 		Xdyad = NULL, Xrow = NULL, Xcol = NULL,
 		model='nrm', symmetric=TRUE, R=2,
-		seed=6886, nscan=3000, burn=1500, odens=1,
+		seed=6886, nscan=3000, burn=2500, odens=1,
 		plot=FALSE, print = FALSE
 		)
-	save(fit, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_defEntAlly.rda'))
+	save(fit$ulAll, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_defEntAlly.rda'))
 }
 
 # Free my clusters
@@ -42,10 +42,10 @@ foreach(yr = yrs, .packages=c("amen")) %dopar% {
 		Y=amData[[yr]][,,c(1,3)], # 3, specifies any alliance
 		Xdyad = NULL, Xrow = NULL, Xcol = NULL,
 		model='nrm', symmetric=TRUE, R=2,
-		seed=6886, nscan=3000, burn=1500, odens=1,
+		seed=6886, nscan=3000, burn=2500, odens=1,
 		plot=FALSE, print = FALSE
 		)
-	save(fit, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_anyAlly.rda'))
+	save(fit$ulAll, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_anyAlly.rda'))
 }
 
 # Free my clusters

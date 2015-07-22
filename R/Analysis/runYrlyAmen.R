b6de86781268b782be1a183737797e585e5e255d
hermes829 <hermes829@gmail.com>
2015-07-22 00:06:12 -0700

    ame built in burn doesnt work, ext work around

--- R/Analysis/runYrlyAmen.R
+++ R/Analysis/runYrlyAmen.R
@@ -20,14 +20,18 @@ cl = makeCluster(8)
 registerDoParallel(cl)
 yrs = names(amData)
 foreach(yr = yrs, .packages=c("amen")) %dopar% {
+	imp = 3000
+	toBurn = 2501
+	# Run Amen model
 	fit = ameRepNull(
 		Y=amData[[yr]][,,1:2], # 2, specifies defensive-entente alliance
 		Xdyad = NULL, Xrow = NULL, Xcol = NULL,
 		model='nrm', symmetric=TRUE, R=2,
-		seed=6886, nscan=3000, burn=2500, odens=1,
-		plot=FALSE, print = FALSE
-		)
-	save(fit$ulAll, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_defEntAlly.rda'))
+		seed=6886, nscan=imp, burn=toBurn, odens=1,
+		plot=FALSE, print = FALSE )
+	# Save lat space
+	latSpace = fit$ulAll[toBurn:imp]
+	save(latSpace, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_defEntAlly.rda'))
 }
 
 # Free my clusters
@@ -38,14 +42,18 @@ cl = makeCluster(8)
 registerDoParallel(cl)
 yrs = names(amData)
 foreach(yr = yrs, .packages=c("amen")) %dopar% {
+	imp = 3000
+	toBurn = 2501
+	# Run Amen model
 	fit = ameRepNull(
 		Y=amData[[yr]][,,c(1,3)], # 3, specifies any alliance
 		Xdyad = NULL, Xrow = NULL, Xcol = NULL,
 		model='nrm', symmetric=TRUE, R=2,
-		seed=6886, nscan=3000, burn=2500, odens=1,
-		plot=FALSE, print = FALSE
-		)
-	save(fit$ulAll, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_anyAlly.rda'))
+		seed=6886, nscan=imp, burn=toBurn, odens=1,
+		plot=FALSE, print = FALSE )
+	# Save lat space
+	latSpace = fit$ulAll[toBurn:imp]
+	save(latSpace, file=paste0(pathResults, 'ameLatSpace/',yr,'_agree3un_anyAlly.rda'))
 }
 
 # Free my clusters

88e0aee8253f9cc01c5800faab3700cf5ded1b31
hermes829 <hermes829@gmail.com>
2015-07-24 15:39:24 -0700

    things are going in the right direction

---
+++ R/Analysis/logitMod.R
@@ -0,0 +1,63 @@
+if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
+	source('~/Research/Carbon/R/setup.R') }
+############################
+
+############################
+# Load necessary files
+load(paste0(pathDataBin, 'repdata.RDA')) # includes object called data
+load(paste0(pathResults, 'latDist.rda')) # includes object called latDist
+load(paste0(pathDataBin, 'idPt.rda'))  # includes object called idPt
+############################
+
+############################
+# Merge together
+# Add latent space strat interest measures
+data$unDefEntDist = latDist$unDefEntDist[match(data$id, latDist$dyadid)]
+data$unAnyDist = latDist$unAnyDist[match(data$id, latDist$dyadid)]
+# Add ideal point strat interest measures
+data$idPtDist = idPt$idealpointdistance[match(data$id, idPt$dyadidyr)]
+data$s2un = idPt$s2un[match(data$id, idPt$dyadidyr)]
+data$s3un = idPt$s3un[match(data$id, idPt$dyadidyr)]
+
+# Drop extraneous datasets
+rm(list=c('latDist', 'idPt'))
+############################
+
+############################
+# Run logit model
+
+# Model spec?
+
+# Something simple and quick for now
+slice = data[,c(
+	'ccode1', 'ccode2', 'year', 
+	'mid', 
+	'unDefEntDist', 'unAnyDist',
+	'idPtDist', 's2un', 's3un')]
+slice = na.omit( slice )
+
+# Run models, i know these models are unrealistic
+mod1 = glm(mid~unDefEntDist, data=slice, family='binomial')
+mod2 = glm(mid~unAnyDist, data=slice, family='binomial')
+mod3 = glm(mid~idPtDist, data=slice, family='binomial')
+mod4 = glm(mid~s2un, data=slice, family='binomial')
+mod5 = glm(mid~s3un, data=slice, family='binomial')
+############################
+
+############################
+# Right direction?
+for(ii in 1:5){
+	eval(parse(text=paste0('mod',ii) ) )
+}
+############################
+
+############################
+# Model performance
+loadPkg('separationplot')
+pdf(file=paste0(pathGraphics, 'quickPerfTest.pdf'))
+par(mfrow=c(2,2))
+for(ii in c(1,3:5)){
+	separationplot(pred=eval(parse(text=paste0('mod',ii) ) )$fitted.values, actual=slice$mid, newplot=FALSE)	
+}
+dev.off()
+############################
---
+++ R/Data/getData/getIdealPts.R
@@ -0,0 +1,43 @@
+if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
+	source('~/Research/Carbon/R/setup.R') }
+
+###############################################################
+# Downloaded data manually from: https://dataverse.harvard.edu/dataset.xhtml?persistentId=hdl:1902.1/12379
+## on July 7, 2015: 5pm
+load(paste0(pathDataRaw,'Voeten/Idealpointsdyadicdistance_1.RData')); idPt=x ; rm(list='x')
+
+# Fix Nauru error
+idPt$countryname1[idPt$ccode1==946] = 'Kiribati'
+idPt$countryname2[idPt$ccode2==946] = 'Kiribati'
+###############################################################
+
+###############################################################
+# Match idPt names to panel
+cntries = c(idPt$countryname1, idPt$countryname2) %>% char() %>% unique() %>% data.frame(cntry=.)
+cntries$cname = cname(cntries$cntry)
+
+# Fix few cnames issue so it matches with panel
+cntries$cname[cntries$cntry=="Yemen People's Republic"] = 'S. YEMEN'
+cntries$cname[cntries$cntry=="Yemen PDR (South)"] = 'S. YEMEN'
+cntries$cname[cntries$cntry=="Yugoslavia"] = 'SERBIA'
+cntries$cname[cntries$cntry=="Czechoslovakia"] = 'CZECH REPUBLIC'
+cntries$cname[cntries$cntry=="Germany, East"] = 'German Democratic Republic'
+
+# Add ccode
+cntries$ccode = panel$ccode[match(cntries$cname,panel$cname)]
+
+# Merge updated cname and ccode to idPt
+idPt$cname1 = cntries$cname[match(idPt$countryname1, cntries$cntry)]
+idPt$cname2 = cntries$cname[match(idPt$countryname2, cntries$cntry)]
+idPt$ccode1 = cntries$ccode[match(idPt$countryname1, cntries$cntry)]
+idPt$ccode2 = cntries$ccode[match(idPt$countryname2, cntries$cntry)]
+
+# Check for duplicates
+idPt$dyadidyr = paste(idPt$ccode1, idPt$ccode2, idPt$year, sep='_')
+stopifnot( length( table(idPt$dyadidyr)[table(idPt$dyadidyr)>1] ) == 0 )
+###############################################################
+
+###############################################################
+# Save
+save(idPt, file=paste0(pathDataBin, 'idPt.rda'))
+###############################################################

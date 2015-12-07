eb1435c181d2db96d8a54612769b0ff2cadb9949
hermes829 <hermes829@gmail.com>
2015-12-07 12:09:22 -0800

    updates to shell script

--- R/runScripts.sh
+++ R/runScripts.sh
@@ -6,9 +6,12 @@ Rscript Data/getData/getVoeten_UN.R
 # Buld data for amen model, run with IGO separately because of temporal discrepancy
 Rscript Data/buildAmenData.R
 Rscript Data/buildAmenData_wIGO.R
+Rscript Data/buildAmenData_idPt_sScore.R
 
 # Run Amen model and latent space anlysis
 Rscript Analysis/runYrlyAmen.R
 Rscript Analysis/getLatDist.R
 Rscript Analysis/runYrlyAmen_wIGO.R
 Rscript Analysis/getLatDist_wIGO.R
+Rscript Analysis/runYrlyAmen_idPt_sScore.R
+Rscript Analysis/getLatDist_idPt_sScore.R

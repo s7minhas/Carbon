c26c7bc98e37782c31fb79ae41ef7ef889a1876a
mgallop <max.gallop@gmail.com>
2016-06-14 09:51:08 -0400

    Ran Logistic Regressions with ICEWS

--- R/Analysis/ameNull/latDistHelpers.R
+++ R/Analysis/ameNull/latDistHelpers.R
@@ -1,5 +1,6 @@
 # Wrapper for all the functions in script
 getLatDist = function(file, label, labelName, symmetric=TRUE){
+	if(symmetric){
 	load(file) # Loads object called latSpace
 	pzMu=getPosInSpace(out[['ULUPM']])
 	latDist=getDyadDist(pzMu, ids=rownames(pzMu))
@@ -9,7 +10,25 @@ getLatDist = function(file, label, labelName, symmetric=TRUE){
 	names(res) = c('ccode1', 'ccode2', 'dist')
 	res = cbind(res, label)
 	names(res)[ncol(res)] = labelName
-	return(res)
+	return(res)}
+	if(!symmetric){
+		load(file) # Loads object called latSpace
+	pzMu=getPosInSpace(fit[['UVPM']], symmetric = FALSE)
+	latDist.U=getDyadDist(pzMu$U, ids=rownames(pzMu$U))
+	latDist.V=getDyadDist(pzMu$V, ids=rownames(pzMu$U))
+
+	latDist.U=stdize(latDist.U, divMean=FALSE)
+	latDist.V=stdize(latDist.V, divMean=FALSE)
+
+	res.U = melt(latDist.U)
+	res.V = melt(latDist.V)
+	res.U$value[which(res.U$Var1 == res.U$Var2)] = res.V$value[which(res.V$Var1 == res.V$Var2)]  = NA
+	res = cbind(res.U, res.V$value)
+	names(res) = c('ccode1', 'ccode2', 'dist.U', 'dist.V')
+	res = cbind(res, label)
+	names(res)[ncol(res)] = labelName
+	return(res)	
+	}
 }
 
 # Procrustes transformation: rotation and reflection
@@ -21,7 +40,8 @@ proc.rr = function(Y,X){
 	t(t(X)%*%Y%*%solve(Ahalf)%*%t(Y)) }
 
 # Get latent space positions
-getPosInSpace = function(latObject){
+getPosInSpace = function(latObject, symmetric = TRUE){
+if(symmetric){
 # ULUPM = latObject[[length(latObject)]]$'ULUPM'
 ULUPM = latObject
 eULU = eigen(ULUPM)
@@ -29,7 +49,18 @@ eR<- which( rank(-abs(eULU$val),ties.method="first") <= 2 )
 U<-eULU$vec[,seq(1,2,length=2),drop=FALSE] %*% sqrt(diag(eULU$val[1:2]))
 L<-eULU$val[eR]   
 rownames(U)<-dimnames(ULUPM)[[1]]
-return(U)
+return(U)}
+if(!symmetric){
+	UVPM = latObject
+	R = 2
+    UDV<-svd(UVPM)
+    U<-UDV$u[,seq(1,R,length=R)]%*%diag(sqrt(UDV$d)[seq(1,R,length=R)],nrow=R)
+    V<-UDV$v[,seq(1,R,length=R)]%*%diag(sqrt(UDV$d)[seq(1,R,length=R)],nrow=R)
+    rownames(U)<-rownames(V)<-dimnames(UVPM)[[1]]
+    return(list(U = U, V = V))
+}
+
+}
 
 	# # Convert to array format
 	# ## didn't do this earlier because arrays take up more space than lists
--- R/Analysis/latSpaceDesc.R
+++ R/Analysis/latSpaceDesc.R
@@ -1,10 +1,14 @@
 if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
 	source('~/Research/Carbon/R/setup.R') }
+
+if(Sys.info()["user"]=="maxgallop"){
+	source('~/Documents/Carbon/R/setup.R') }
+
 ############################
 
 ############################
 # Get dyadic dist from each yrly latent space
-yrs=1946:2011
+yrs=2001:2011
 ############################
 
 ############################
@@ -13,8 +17,8 @@ yrs=1946:2011
 source( paste0(gpth, 'R/Analysis/ameNull/latDistHelpers.R') )
 
 # Files to pull
-outNameMeanRepl = '_idPt_sScore_MeanRepl.rda'
-latRdas = paste0(pathResults, 'ameLatSpace') %>% list.files() %>% .[grepl('_idPt_sScore_', .)] %>% paste0(pathResults, 'ameLatSpace/', .)
+outNameMeanRepl = '_icews.rda'
+latRdas = paste0(pathResults, 'ameLatSpace') %>% list.files() %>% .[grepl('icews', .)] %>% paste0(pathResults, 'ameLatSpace/', .)
 tmp = paste0(pathResults, 'ameLatSpace/', yrs, outNameMeanRepl)
 setdiff( tmp, latRdas ) # Check to make sure all files exist
 
@@ -24,12 +28,12 @@ latSpaces = lapply(yrs, function(yr){
 	labelName='year'
 	symmetric=TRUE
 	load(file) # Loads object called latSpace
-	pzMu=getPosInSpace(out[['ULUPM']])
+	pzMu=getPosInSpace2(fit[['UVPM']])
 	return(pzMu)
 	})
 
-latSpaceSumm = lapply(1:length(latSpaces), function(ii){ 
-	x = latSpaces[[ii]]
+latSpaceSumm1 = lapply(1:length(latSpaces), function(ii){ 
+	x = latSpaces[[ii]][[1]]
 	summ = apply(x, 2, function(x){ c(min(x), mean(x), max(x)) })
 	colnames(summ) = c('d1', 'd2')
 	summD = data.frame(summ)
@@ -39,14 +43,25 @@ latSpaceSumm = lapply(1:length(latSpaces), function(ii){
 	return(summD)
 	}) %>% do.call('rbind', .)
 
-lsDims = ggplot(latSpaceSumm, aes(x=year, y=value, color=variable)) + geom_point() + geom_line() + facet_wrap(variable~var, ncol=3, scales='free') 
+latSpaceSumm2 = lapply(1:length(latSpaces), function(ii){ 
+	x = latSpaces[[ii]][[2]]
+	summ = apply(x, 2, function(x){ c(min(x), mean(x), max(x)) })
+	colnames(summ) = c('d1', 'd2')
+	summD = data.frame(summ)
+	summD$var = c('min', 'mean', 'max')
+	summD = melt(summD, id='var')
+	summD$year = yrs[ii]
+	return(summD)
+	}) %>% do.call('rbind', .)
+
+lsDims = ggplot(latSpaceSumm1, aes(x=year, y=value, color=variable)) + geom_point() + geom_line() + facet_wrap(variable~var, ncol=3, scales='free') 
 lsDims
 
-rownames(latSpaces[[1]])
+rownames(latSpaces[[1]][[1]])
 
 latSpaceData = lapply(1:length(latSpaces), function(ii){
-	x = latSpaces[[ii]]
-	colnames(x) = c('d1', 'd2')
+	x = cbind(latSpaces[[ii]][[1]], latSpaces[[ii]][[2]])
+	colnames(x) = c('d1U', 'd2U', 'd1V', 'd2V')
 	xD = data.frame(x, row.names=NULL)
 	xD$cntry = rownames(x)
 	# xD = melt(xD, id='cntry')
@@ -54,5 +69,5 @@ latSpaceData = lapply(1:length(latSpaces), function(ii){
 	return(xD)
 	}) %>% do.call('rbind', .)
 
-lsPlot = ggplot(latSpaceData, aes(x=d1, y=d2, label=cntry, color=factor(cntry))) + geom_text() + facet_wrap(~year, ncol=3, scales='free') + theme(legend.position='none')
+lsPlot = ggplot(latSpaceData, aes(x=d1U, y=d2U, label=cntry, color=factor(cntry))) + geom_text() + facet_wrap(~year, ncol=3, scales='free') + theme(legend.position='none')
 ggsave(lsPlot, file='~/Desktop/lsPlot.pdf', height=100, width=100, limitsize=FALSE)
